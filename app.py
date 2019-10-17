5e38794fdb4892fc272b2d74a588033b479503a9
Alanna <alanna.noguchi@students.makeschool.com>
2019-10-17 11:21:26 -0700

    Added button functionality to Player Hub

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/courts')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
-items = db.items
+players = db.players
+
 
 @app.route('/')
 def index():
@@ -24,7 +25,26 @@ def signup():
     return render_template("signup.html", msg="Create New Player")
 
 
+@app.route('/playerhub', methods=['POST'])
+def signup_submit():
+    """Submit new player."""
+    player = {
+        'first name':request.form.get('first name'),
+        'last name': request.form.get('last name'),
+        'city':request.form.get('city'),
+        'state':request.form.get('state')
+    }
+    player_id = players.insert_one(player).inserted_id
+    return redirect(url_for('playerhub_show.html', player_id=player_id))
 
+
+# This route will show one item
+@app.route('/playerhub/<player_id>')
+def playerhub_show(player_id):
+    """Show the player hub."""
+    # item = items.find_one({'_id': ObjectId(item_id)})
+    player = players.find_one({'_id': ObjectId(player_id)})
+    return render_template('playerhub_show', player=player)
 
 
 if __name__ == '__main__':
