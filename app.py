bef5f9a540f9c782d2af8ebcc5cfc3d86bbeb2d3
Alanna <alanna.noguchi@students.makeschool.com>
2019-10-16 18:09:44 -0700

    Updated

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import os
 
 
 app = Flask(__name__)
+
+
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/courts')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
+items = db.items
 
 @app.route('/')
 def index():
@@ -13,3 +22,10 @@ def index():
 def signup():
     """Redirects user to sign up page"""
     return render_template("signup.html", msg="Create New Player")
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
